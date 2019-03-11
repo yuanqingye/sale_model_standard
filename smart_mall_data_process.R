e183f8c747b500f3a01349a1479c9f801dc5aa3c
yuanqingye <yuan.qingye@yahoo.com>
2019-03-11 18:03:27 +0800

    sale_model_weekly_commit_2019_0311
    weekly commit

--- smart_mall_data_process.R
+++ smart_mall_data_process.R
@@ -39,6 +39,30 @@ for(dt in as.character(20190201:20190209))
   save(smart_mall_track_data_list,file = "./smart_mall_track_data_list_second.RData")
 }
 
+#options(java.parameters = "-Xmx1024m")
+source('~/Rfile/R_impala.R',encoding = 'UTF-8')
+jgc <- function()
+{
+  gc()
+  .jcall("java/lang/System", method = "gc")
+}
+
+for(dt in as.character(20190126:20190131))
+{
+  smart_mall_track_data_sql = paste0("select * from ods.ods_wisdowmall_store_track_dt where dt = '",dt,"'")
+  smart_mall_track_data_list[[dt]] = read_data_impala_general(smart_mall_track_data_sql)
+  save(smart_mall_track_data_list,file = "./smart_mall_track_data_list_second.RData")
+  jgc()
+}
+
+for(dt in as.character(20190201:20190209))
+{
+  smart_mall_track_data_sql = paste0("select * from ods.ods_wisdowmall_store_track_dt where dt = '",dt,"'")
+  smart_mall_list[[dt]] = read_data_impala_general(smart_mall_track_data_sql)
+  save(smart_mall_list,file = "./smart_mall_list.RData")
+  jgc()
+}
+
 View(smart_mall_track_data_list[["20190210"]])
 
 smart_mall_list = list()
@@ -111,4 +135,83 @@ boxplot(mix_uv_sale_normalization[["February"]]$pv_over_sale_count)
 boxplot(mix_uv_sale_normalization[["February"]]$uv_over_sale_sum)
 boxplot(mix_uv_sale_normalization[["February"]]$pv_over_sale_sum)
 
+library(dbscan)
+kNNdist(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")],k = 5)
+kNNdistplot(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")], k = 5)
+abline(h=1.3, col = "red", lty=2)
+
+dbscan_clust = dbscan(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")], eps = 1.3, minPts = 5)
+mix_uv_sale_normalization[["February"]]$dbscan_prop = dbscan_clust$cluster
+#in this method we got cluster number 2 and the cluster result
+
+library(mclust)
+m_clust <- Mclust(as.matrix(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")]), G=1:20) #聚类数目从1一直试到20
+summary(m_clust)
+
+plot(m_clust, "BIC")
+
+#2.Using kmeans and using inner method/index to tell which cluster is best
+library(NbClust)
+set.seed(1234) #因为method选择的是kmeans，所以如果不设定种子，每次跑得结果可能不同
+nb_clust <- NbClust(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")],  distance = "euclidean",
+                    min.nc=2, max.nc=15, method = "kmeans",
+                    index = "alllong", alphaBeale = 0.1)
+#best cluster 2
+
+wssplot <- function(data, nc=15, seed=1234){
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  for (i in 2:nc){
+    set.seed(seed)
+    wss[i] <- sum(kmeans(data, centers=i)$withinss)
+  }
+  plot(1:nc, wss, type="b", xlab="Number of Clusters",
+       ylab="Within groups sum of squares")}
+
+wssplot(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")])
+#best cluster 2
+
+library(factoextra)
+library(ggplot2)
+set.seed(1234)
+fviz_nbclust(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")], kmeans, method = "wss") +
+  geom_vline(xintercept = 2, linetype = 2)
+
+km.res <- kmeans(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")], 2)
+fviz_cluster(km.res, data = mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")])
+#best cluster 2
+
+#4.K Medoids clustering
+library(fpc)
+pamk.best <- pamk(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")])
+pamk.best$nc
+
+library(cluster)
+clusplot(pam(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")], pamk.best$nc))
+#best cluster 2
+
+#5.Clinsky criterion
+library(vegan)
+ca_clust <- cascadeKM(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")],1, 10, iter = 1000)
+ca_clust$results
+
+#6.Affinity propagation (AP) clustering
+library(apcluster)
+ap_clust <- apcluster(negDistMat(r=2), mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")])
+length(ap_clust@clusters)
+
+#best cluster 13
+
+#7. 轮廓系数Average silhouette method
+require(cluster)
+library(factoextra)
+fviz_nbclust(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")], kmeans, method = "silhouette")
+
+#best cluster 3
+
+#8. Gap Statistic
+library(cluster)
+set.seed(123)
+gap_clust <- clusGap(mix_uv_sale_normalization[["February"]][,c("uv_over_sale_count","pv_over_sale_count","uv_over_sale_sum","pv_over_sale_sum")], kmeans, 10, B = 500, verbose = interactive())
+gap_clust
+#best cluster 3
 
